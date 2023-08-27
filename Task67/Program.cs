c4acb16cf5df644d7939ce774df7da4b6d79f0d1
Елена <lenna@gmail.com>
2023-08-27 18:10:24 +0300

    Added program in Task67

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+// int FactorialRec(int n)
+// {
+// if (n == 1) return 1;
+// else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialRec(10)); // 3628800
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = SumNaturalNambers(number);
+Console.WriteLine(sum);
+
+int SumNaturalNambers(int n)
+{
+    if (n == 0) return 0;
+
+    return n % 10 + SumNaturalNambers(n / 10);
+
+}
+
